b1f6afb67dc276059aad054c46cfad393bb0fc3c
SamTechnologeek <linux.unix.4ever@gmail.com>
2012-12-06 10:47:27 +0000

    Separated console.c from include/ to lib/

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ BUILD=build
 
 all:
 	nasm $(ASMFLAGS) $(BUILD)/loader.o boot/loader.s	
-	gcc -c -Iinclude include/sys/console.c -o $(BUILD)/console.o  $(CFLAGS)
+	gcc -c -Iinclude lib/console.c -o $(BUILD)/console.o  $(CFLAGS)
 	gcc -c -Iinclude kernel/kernel.c -o $(BUILD)/kern.o $(CFLAGS)
 	ld $(LDFLAGS) kernel/kernel.bin $(BUILD)/console.o $(BUILD)/kern.o $(BUILD)/loader.o
 
---
+++ lib/console.c
@@ -0,0 +1,54 @@
+/* console.h implementation */
+
+#include <sys/console.h>
+
+#define screen_width 120
+#define screen_height 70
+Uint8 *ram = (Uint8 *)0xB8000; /* Video ram */
+Upoint2d cursor = {0,0};
+
+void clear_screen()
+{
+  unsigned int i = 0;
+  while(i<=screen_width*screen_height)
+    {
+      ram[i] = 0;
+      i++;
+    }
+  cursor.x = 0;
+  cursor.y = 0;
+}
+
+void print_string(string str, int8 color)
+{
+  int i;
+  for(i=0;str[i]!=0;++i)
+    {
+      print_char(str[i],color);
+    }
+}
+
+void print_char(char c, int8 color)
+{
+  unsigned int i = cursor.x+(screen_width*cursor.y);
+  ram[i] = c;
+  ram[i+1] = color;
+  cursor.x = cursor.x+2;
+  update_cursor_y();
+}
+
+void update_cursor_y() /* Used to update the Y coordinate of the cursor. */
+{
+  if(cursor.x/2>=screen_width)
+    {
+      cursor.y++;
+      cursor.x = cursor.x - screen_width*2;
+      update_cursor_y();
+    }
+}
+
+void move_cursor(int x,int y)
+{
+  cursor.x = x*2;
+  cursor.y = y*2;
+}
